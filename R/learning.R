864dd1b12c8e333ac29ed9ec0663939ad6043159
Olivia Klatt <olivia.klatt@scilifelab.se>
2023-01-16 16:05:02 +0100

    saving the file into the history for the first time

---
+++ R/learning.R
@@ -0,0 +1,20 @@
+10
+
+# R basics ----------------------------------------------------------------
+
+weight_kilos <- 100
+weight_kilos <- 10
+weight_kilos
+colnames(airquality)
+str(airquality)
+summary(airquality)
+
+2 + 2
+
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+# run is command+enter
+?colnames
+help(colnames)
