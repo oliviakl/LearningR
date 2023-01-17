ca3c5ec9b8f8c2147718ec0e48a25fe02755d98f
Olivia Klatt <olivia.klatt@scilifelab.se>
2023-01-17 14:14:12 +0100

    Showing off summarize

--- R/learning.R
+++ R/learning.R
@@ -150,16 +150,44 @@ nhanes_small %>%
 
 # 1. BMI between 20 and 40 with diabetes
 nhanes_small %>%
-    # Format should follow: variable >= number or character
-    filter(bmi >= 20 & bmi <= 40 & diabetes == "Yes")
+  # Format should follow: variable >= number or character
+  filter(bmi >= 20 & bmi <= 40 & diabetes == "Yes")
 
 # Pipe the data into mutate function and:
 nhanes_modified <- nhanes_small %>% # Specifying dataset
-    mutate(
-        # 2. Calculate mean arterial pressure
-        mean_art_pres = (((2 * bp_dia_ave) + bp_sys_ave) / 3),
-        # 3. Create young_child variable using a condition
-        young_child = if_else( age < 6, "Yes", "No")
-    )
+  mutate(
+    # 2. Calculate mean arterial pressure
+    mean_art_pres = (((2 * bp_dia_ave) + bp_sys_ave) / 3),
+    # 3. Create young_child variable using a condition
+    young_child = if_else(age < 6, "Yes", "No")
+  )
 
 nhanes_modified
+
+# Summarizing  ------------------------------------------------------------
+
+nhanes_small %>%
+  summarise(
+    max_bmi = max(bmi)
+  )
+# summarize will only give you one row unless xou group it NA because missing values, to deal with that use na.rm = TRUE
+nhanes_small %>%
+  summarise(
+    max_bmi = max(bmi, na.rm = TRUE)
+  )
+# only gives what Im asking for
+nhanes_small %>%
+  filter(!is.na(diabetes)) %>%
+  group_by(
+    diabetes,
+    phys_active
+  ) %>%
+  summarize(
+    max_bmi = max(bmi,
+      na.rm = TRUE
+    ),
+    min_bmi = min(bmi,
+      na.rm = TRUE
+    )
+  )
+# group_by to use split and combine for diabetes and phys_active
