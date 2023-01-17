bd2d8f1d52265aabe610a001e4727e15d4576d01
Olivia Klatt <olivia.klatt@scilifelab.se>
2023-01-17 11:16:00 +0100

    Made smaller nhanes dataset

--- R/learning.R
+++ R/learning.R
@@ -17,7 +17,33 @@ summary(airquality)
 # Packages ----------------------------------------------------------------
 
 library(tidyverse)
+library(NHANES)
+
 # run is command+enter
 ?colnames
 help(colnames)
-#This will be used for testing out Git
+# This will be used for testing out Git
+
+
+# Looking at data ---------------------------------------------------------
+
+glimpse(NHANES)
+# glimpse will let you see the data in a specific way
+select(NHANES, Age)
+# dataset itself is not being touched, only showing Age of dataset
+
+select(NHANES, Age, Weight, BMI)
+
+select(NHANES, -HeadCirc)
+# useful when I dont need all the variables
+
+select(NHANES, starts_with("BP"))
+# selects all the varibales that satrt with "BP"
+
+select(NHANES, ends_with("Day"))
+#selects data that ends with Day
+select(NHANES, contains("Age"))
+
+nhanes_small <- select(NHANES, Age, Gender, BMI, Diabetes, PhysActive, BPSysAve, BPDiaAve, Education)
+#creates new object that only includes Age, Gender etc form the data of NHANES
+nhanes_small
